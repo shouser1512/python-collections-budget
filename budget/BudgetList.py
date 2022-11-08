86a4bd01c06d408b07fdd06c3661f2ada94dac9c
shouser1512 <saxena.shobhit1512@gmail.com>
2022-11-08 10:55:59 -0500

    Update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,9 +1,7 @@
-
 from . import Expense
 
-class BudgetList:
-
-    def BudgetList(self, budget):
+class BudgetList():
+    def __init__(self, budget):
         self.budget = budget
         self.sum_expenses = 0
         self.expenses = []
@@ -11,7 +9,6 @@ class BudgetList:
         self.overages = []
     
     def append(self, item):
-        
         if (self.sum_expenses + item) < self.budget:
             self.expenses.append(item)
             self.sum_expenses += item
@@ -22,13 +19,13 @@ class BudgetList:
     def __len__(self):
         return (len(self.expenses) + len(self.overages))
 
-    def main():
-        myBudgetList = BudgetList(1200)
-        expenses = Expense.Expenses()
-        expenses.read_expenses('data/spending_data.csv')
-        for expense in expenses.list:
-            myBudgetList.append(expense.amount)
-        print('The count of all expenses: ' + str(len(myBudgetList)))
-    
-    if __name__ == "__main__":
-        main()
+def main():
+    myBudgetList = BudgetList(1200)
+    expenses = Expense.Expenses()
+    expenses.read_expenses('data/spending_data.csv')
+    for expense in expenses.list:
+        myBudgetList.append(expense.amount)
+    print('The count of all expenses: ' + str(len(myBudgetList)))
+
+if __name__ == "__main__":
+    main()
